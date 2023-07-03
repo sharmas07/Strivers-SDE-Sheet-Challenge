44d8bcbd03f2ab3cfa8fd1786b6d8b0e0389b86d
sharmas07 <valis6435@gmail.com>
2023-07-03 12:50:31 +0530

    day#27

---
+++ Day-27/palindromi-partitioning.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+bool isPalindrome(string s, int start, int end) {
+  while (start <= end) {
+    if (s[start++] != s[end--]) {
+      return false;
+    }
+  }
+  return true;
+}
+void partition(int index, string &s, vector<string> &path,
+               vector<vector<string>> &ans) {
+  if (index == s.size()) {
+    ans.push_back(path);
+    return;
+  }
+  for (int i = index; i < s.size(); i++) {
+    if (isPalindrome(s, index, i)) {
+      path.push_back(s.substr(index, i - index + 1));
+      partition(i + 1, s, path, ans);
+      path.pop_back();
+    }
+  }
+}
+
+vector<vector<string>> partition(string &s) {
+  vector<vector<string>> ans;
+  vector<string> path;
+  partition(0, s, path, ans);
+  return ans;
+}
